34e65f8d20a74fcf701a69a2c2676cd1d42b47f1
Suliman <sulimanrageb015@gmail.com>
2026-08-30 00:22:55 +0300

    Q02

---
+++ Assignment Advanced C# 01/Container.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_Advanced_C__01
+{
+    internal class Container<T>
+    {
+        public T Add(T item)
+        {
+            return item;
+        }
+        public T Get(T item)
+        {
+            return item;
+        }
+    }
+}
