808e414dcafa0d3a06ea1161bfe3e1fb1d57d548
Suliman <sulimanrageb015@gmail.com>
2026-08-30 00:11:35 +0300

    Q01

--- Assignment Advanced C# 01/Program.cs
+++ Assignment Advanced C# 01/Program.cs
@@ -1,1 +1,5 @@
 ﻿
+#region Q01
+//Q1: What is a generic class? Why use generics? 
+//الgeneric class هي Class بتحدد نوع العامل بدل نوع بينات ثابت وبنسخدمها لأن ال generic class تقدر تتعامل مع اي نوع بينات مختلف بدل نوع بينات ثابت 
+#endregion
