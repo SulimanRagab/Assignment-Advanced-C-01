49552bfcf29d61fc634ac8dd33c79b50ba452298
Suliman <sulimanrageb015@gmail.com>
2026-08-30 12:08:44 +0300

    Q04 Generic Method

---
+++ Assignment Advanced C# 01/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_Advanced_C__01
+{
+    internal class Helper
+    {
+       public static void Swap<T>(T a ,T b)
+       {
+            T temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine($"a: {a} , b: {b}");
+       }
+
+    }
+}
--- Assignment Advanced C# 01/Program.cs
+++ Assignment Advanced C# 01/Program.cs
@@ -8,3 +8,12 @@
 // Q3:What are multiple type parameters? Write Pair<TKey,TValue >.
 // ال  multiple type معناها ببساطه ان ال Class  تقدر تستخدم  اتنين او اكثر Type Parameter بدل Type Parameter واحد 
 #endregion
+
+#region Q04
+// Q4: What is a generic method? Write Swap<T> method.
+// ال Generic Method هيا ميثود بتستخدم معامل من نوع واحد مثال public static Metho<T>(T a) او اكثر من نوع ودا يخليها تقدر تتعامل مع اكثر من نوع بينات بدل نوع واحد ثابت  
+//using Assignment_Advanced_C__01;
+//int a = 1;
+//int b = 2;
+//Helper.Swap<int>(a, b);
+#endregion
